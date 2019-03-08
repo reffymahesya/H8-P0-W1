b049c0e8497dfa585b9cba4e53c42d2a4892e5fd
reffymahesya <reffymahesyar@gmail.com>
2019-03-08 16:39:26 +0700

    Tugas JavaScript6 - Asterisk

---
+++ JavaScript6/JavaScript6.js
@@ -0,0 +1,32 @@
+//ROWS 1
+var rows1 = 5
+
+for (i=0; i<rows1; i++){
+  console.log('*')
+}
+
+//ROWS 2
+var rows2 = 5
+var cols2 = 1
+var bintang = ''
+
+for (m=0; m<rows2; m++){
+  bintang = ''
+  for (n=0; n<5; n++){
+  bintang += '*' 
+  }
+  console.log(bintang)
+}
+
+
+//ROWS 3
+var rows3 = 5
+var cols3 = 1
+var bintang = ''
+
+for (k=0; k<rows3; k++){
+  for (l=0; l<cols3; l++){
+  bintang += '*' 
+  }
+  console.log(bintang) 
+}
