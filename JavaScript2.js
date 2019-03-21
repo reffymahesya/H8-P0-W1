72d8244ddc7f4cb3a6a188a2fd6a017c6970e0da
reffymahesya <reffymahesyar@gmail.com>
2019-03-21 10:32:56 +0700

    revisi validasi

--- JavaScript2.js
+++ JavaScript2.js
@@ -1,24 +1,21 @@
-var nama = 'Bejo'
-var peran = ''
+var nama = 'Reffy'
+var peran = 'Gunner'
 
   if (nama === ''){
   	console.log('Nama harus diisi!')
 }
-  else if (nama === 'Mikael'){
-	console.log('Halo Mikael, Pilih Peranmu untuk memulai game!')
-} else if(nama === 'Nina'){
-	console.log('Selamat datang di Dunia Proxytia, Nina')
-	peran = 'Ksatria'
-	console.log('Halo Ksatria Nina, kamu dapat menyerang dengan senjatamu!')
-} else if(nama === 'Danu'){
-	console.log('Selamat datang di Dunia Proxytia, Danu')
-	peran = 'Tabib'
-	console.log('Halo Tabib Danu, kamu akan membantu temanmu yang terluka.')
-} else if(nama === 'Zero'){
-	console.log('Selamat datang di Dunia Proxytia, Zero')
-	peran = 'Penyihir'
-	console.log('Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu!')
+  else if (peran === ''){
+	console.log('Halo, ' + nama + ' Pilih Peranmu untuk memulai game!')
+} else if(peran === 'Ksatria'){
+	console.log('Selamat datang di Dunia Proxytia, ' + nama)
+	console.log('Halo Ksatria' + nama + ', kamu dapat menyerang dengan senjatamu!')
+} else if(peran === 'Tabib'){
+	console.log('Selamat datang di Dunia Proxytia, ' + nama)
+	console.log('Halo Tabib ' + nama + ', kamu akan membantu temanmu yang terluka.')
+} else if(peran === 'Penyihir'){
+	console.log('Selamat datang di Dunia Proxytia, ' + nama)
+	console.log('Halo Penyihir'  + nama + ', ciptakan keajaiban yang membantu kemenanganmu!')
 } else {
-	console.log('Karakter tidak tersedia dalam Dunia Proxytia!')
+	console.log('Peran tidak tersedia dalam Dunia Proxytia!')
 }
 
